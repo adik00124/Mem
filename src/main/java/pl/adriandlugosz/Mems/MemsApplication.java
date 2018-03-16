f68762e320224c3f6c8b390c522af3264bb11d10
Adrian Długosz <dlugosz.ad@gmail.com>
2018-03-16 17:48:12 +0100

    start project

---
+++ src/main/java/pl/adriandlugosz/Mems/MemsApplication.java
@@ -0,0 +1,12 @@
+package pl.adriandlugosz.Mems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemsApplication.class, args);
+	}
+}
