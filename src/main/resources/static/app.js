81b1bf5538713bd4fbef2cd90224ed0291a6436a
Adrian Długosz <dlugosz.ad@gmail.com>
2018-03-21 18:00:10 +0100

    copy button added

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,3 +2,11 @@ $(function(){
     // Activate mobile nav toggle button
     $(".button-collapse").sideNav({edge: 'right'});
 });
+
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
